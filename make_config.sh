47d7a19008941283001876a39adcf0d2920cd94f
Gabriel Oana <gabriel.oana91@gmail.com>
2020-04-26 23:26:46 +0100

    [ADD] Deploy build to test

--- make_config.sh
+++ make_config.sh
@@ -5,16 +5,16 @@ live_pass=$2
 test_user=$3
 test_pass=$4
 
-echo "[distutils]" >> .pypirc
-echo "index-servers=" >> .pypirc
-echo "  pypi" >> .pypirc
-echo "  testpypi" >> .pypirc
-echo "" >> .pypirc
-echo "[pypi]" >> .pypirc
-echo "username="$1 >> .pypirc
-echo "password="$2 >> .pypirc
-echo "" >> .pypirc
-echo "[testpypi]" >> .pypirc
-echo "repository=https://test.pypi.org/legacy/" >> .pypirc
-echo "username="$3 >> .pypirc
-echo "password="$4 >> .pypirc
+echo "[distutils]" >> ~/.pypirc
+echo "index-servers=" >> ~/.pypirc
+echo "  pypi" >> ~/.pypirc
+echo "  testpypi" >> ~/.pypirc
+echo "" >> ~/.pypirc
+echo "[pypi]" >> ~/.pypirc
+echo "username="$1 >> ~/.pypirc
+echo "password="$2 >> ~/.pypirc
+echo "" >> ~/.pypirc
+echo "[testpypi]" >> ~/.pypirc
+echo "repository=https://test.pypi.org/legacy/" >> ~/.pypirc
+echo "username="$3 >> ~/.pypirc
+echo "password="$4 >> ~/.pypirc
